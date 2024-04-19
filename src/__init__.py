875d4e82181c1677506565564b84ae24731bac59
Aryan Mohanty <aryan.21bce8377@vitapstudent.ac.in>
2024-04-19 20:37:19 +0530

    add mapping instruction

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+from pyswmm import Simulation
+import swmmio
+
+# The Input Area image is present in the file src directory.
+with Simulation('src\input.inp') as sim:
+    for step in sim:
+        pass
+
+crs = 'epsg:3728' # Coordinate Reference System
+simulation_info = swmmio.Model("src\input.inp", crs=crs)
+
+# Here is the mapped control flow for the presented area in interactive HTML format.
+swmmio.create_map(simulation_info, filename="outputMap.html")
