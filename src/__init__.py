8829b29ca76bbe6e1cdcfa2d1c0dde104ba9ee42
Aryan Mohanty <aryan.21bce8377@vitapstudent.ac.in>
2024-04-19 20:40:26 +0530

    add the output sample image

--- src/__init__.py
+++ src/__init__.py
@@ -10,4 +10,5 @@ crs = 'epsg:3728' # Coordinate Reference System
 simulation_info = swmmio.Model("src\input.inp", crs=crs)
 
 # Here is the mapped control flow for the presented area in interactive HTML format.
+# A sample picture for the same has been added to the file structure as well.
 swmmio.create_map(simulation_info, filename="outputMap.html")
